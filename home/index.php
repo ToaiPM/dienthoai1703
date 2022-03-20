e94f1fee9da621bc3326ac0ee537dcfdda6b4964
nttoai <nttoai@gmail.com>
2022-03-19 21:32:27 -0700

    19-3-2022

--- home/index.php
+++ home/index.php
@@ -5,44 +5,43 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="icon" href="/public/img/icon/link_icon.jpg" type="image/gif" sizes="16x16">
-    <link rel="stylesheet" href="/public/css/style.css">
     <link rel="stylesheet" href="/public/css/grid.css">
+    <link rel="stylesheet" href="/public/css/style.css">
     <link rel="stylesheet" href="/public/css/all.min.css">
+    <script src="/public/js/jquery.min.js"></script>
+    <script>
+        $(document).ready(function() {
+            function DanhSach(){
+                $.ajax({
+                    type: 'POST',
+                    url: '/home/home_danh_sach.php',
+                    data: {
+                    },
+                    dataType: 'html',
+                    success: function(res){
+                        $('#danhsach').html(res);
+                    }
+                });
+            }
+            DanhSach();
+        });
+    </script>
     <title>Trang home</title>
 </head>
 <body>
-    <div class="header">
-        <div class="main_menu grid wide">
-            <a href="/" class="home_gr">
-                <img src="/public/img/icon/home_icon.png" alt="" class="home_img">
-            </a>
-            <ul>
-                <li>
-                    <a href="#">Giới thiệu</a>
-                </li>
-                <li>
-                    <a href="#">Danh mục <i class="fa-solid fa-caret-down"></i></a>
-                </li>
-                <li>
-                    <a href="#">Tin tức</a>
-                </li>
-                <li>
-                    <a href="#">Liên hệ</a>
-                </li>
-            </ul>
-            <div class="timkiem_gr">
-                <input type="text" class="timkiem_txt" placeholder="Bạn cần tìm sản phẩm nào ...">
-                <button class="timkiem_btn"><i class="fa-solid fa-magnifying-glass"></i></button>
-            </div>
-            <div class="giohang_gr">
-                <i class="fa-solid fa-cart-shopping"></i>
-                <span class="giohang_sl">0</span>
-            </div>
-            <div class="nguoidung_gr">
-                <span class="nguoidung_dn">Đăng nhập</span>
-                <span class="nguoidung_dx">Đăng xuất</span>
-            </div>
-        </div>
+    <!-- Thanh menu -->
+    <?php include_once __SITE_PATH . '/layouts/header.php'; ?>
+
+    <!--banner-->
+    <?php include_once __SITE_PATH . '/layouts/banner.php'; ?>
+
+    <!--Danh sách sản phẩm -->
+    <div class="sanpham_gr">
+        <input type="hidden" id="TrangHienTai" value="1">
+        <div class="grid wide" id="danhsach"></div>
     </div>
+
+    <!-- Footer -->
+    <?php include_once __SITE_PATH . '/layouts/footer.php'; ?>
 </body>
 </html>
---
+++ layouts/header.php
@@ -0,0 +1,39 @@
+<div class="header">
+    <div class="main_menu grid wide">
+        <a href="/" class="home_gr">
+            <img src="/public/img/icon/home_icon.png" alt="" class="home_img">
+        </a>
+        <ul>
+            <li>
+                <a href="#">Giới thiệu</a>
+            </li>
+            <li>
+                <a href="#">Danh mục &nbsp;<i class="fa-solid fa-caret-down"></i></a>
+                <ul class="sub_menu">
+                    <li><a href="#">iPhone</a></li>
+                    <li><a href="#">Samsung</a></li>
+                    <li><a href="#">OPPO</a></li>
+                    <li><a href="#">Xiaomi</a></li>
+                </ul>
+            </li>
+            <li>
+                <a href="#">Tin tức</a>
+            </li>
+            <li>
+                <a href="#">Liên hệ</a>
+            </li>
+        </ul>
+        <div class="timkiem_gr">
+            <input type="text" class="timkiem_txt" placeholder="Bạn cần tìm sản phẩm nào ...">
+            <button class="timkiem_btn"><i class="fa-solid fa-magnifying-glass"></i></button>
+        </div>
+        <div class="giohang_gr">
+            <i class="fa-solid fa-cart-shopping"></i>
+            <span class="giohang_sl">0</span>
+        </div>
+        <div class="nguoidung_gr">
+            <span class="nguoidung_dn">Đăng nhập</span>
+            <span class="nguoidung_dx">Đăng xuất</span>
+        </div>
+    </div>
+</div>
